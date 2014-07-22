44c1ea5009ead916f87bba26c89e20b4fab4e6f5
Kris Moore <kris@pcbsd.org>
2014-07-22 16:08:34 -0400

    Add back the temp fix to pkgng 1.3.0rc4, patch still fails to work

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -313,6 +313,9 @@ cp_iso_pkg_files()
       rc_halt "${PKGSTATIC} -C ${PROGDIR}/tmp/pkg.conf -R ${PROGDIR}/tmp/repo/ fetch -r pcbsd-build -y -o ${PROGDIR}/tmp -d ${pkgName}"
     done
 
+    # Add back the TEMP fix to pkgng 1.3.0rc4
+    mv ${PROGDIR}/tmp/.real_*/All ${PROGDIR}/tmp
+
     # Copy pkgng
     rc_halt "cp ${PROGDIR}/tmp/All/pkg-*.txz ${PROGDIR}/tmp/All/pkg.txz"
 
