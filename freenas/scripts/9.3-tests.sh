46714113813692976fb03532d44931b6d1c1fca4
Kris Moore <kris@pcbsd.org>
2015-06-29 10:01:42 -0400

    Spruce up the text output from running FreeNAS 9.3 tests, group them into categories with a progress indicator

--- freenas/scripts/9.3-tests.sh
+++ freenas/scripts/9.3-tests.sh
@@ -67,6 +67,87 @@ rc_halt()
   fi
 }
 
+set_test_group_text()
+{
+  GROUPTEXT="$1"
+  TOTALTESTS="$2"
+  TCOUNT=0
+}
+
+echo_test_title()
+{
+  TCOUNT=`expr $TCOUNT + 1`
+  echo "Running $GROUPTEXT ($TCOUNT/$TOTALTESTS) -  $1"
+}
+
+storage_tests()
+{
+  # Set the group text and number of tests
+  set_test_group_text "Storage Tests" "4"
+
+  # Check getting disks
+  echo_test_title "Disks / API functionality"
+  GET /storage/disk/ -v 2>${RESTYERR} >${RESTYOUT}
+  check_rest_response "200 OK"
+
+  # Check creating a zpool
+  echo_test_title "Creating volume"
+  POST /storage/volume/ '{ "volume_name" : "tank", "layout" : [ { "vdevtype" : "stripe", "disks" : [ "ada1", "ada2" ] } ] }' -v >${RESTYOUT} 2>${RESTYERR}
+  check_rest_response "201 CREATED"
+
+  # Check creating a dataset
+  echo_test_title "Creating dataset"
+  POST /storage/volume/1/datasets/ '{ "name": "share" }' -v >${RESTYOUT} 2>${RESTYERR}
+  check_rest_response "201 CREATED"
+
+  # Set the permissions of the dataset
+  echo_test_title "Changing permissions"
+  PUT /storage/permission/ '{ "mp_path": "/mnt/tank", "mp_acl": "unix", "mp_mode": "777", "mp_user": "root", "mp_group": "wheel" }' -v >${RESTYOUT} 2>${RESTYERR}
+  check_rest_response "201 CREATED"
+}
+
+nfs_tests()
+{
+  # Set the group text and number of tests
+  set_test_group_text "NFS Tests" "9"
+
+  # Enable NFS server
+  echo_test_title "Creating the NFS server"
+  PUT /services/nfs/ '{ "nfs_srv_bindip": "'"${ip}"'", "nfs_srv_mountd_port": 618, "nfs_srv_allow_nonroot": false, "nfs_srv_servers": 10, "nfs_srv_udp": false, "nfs_srv_rpcstatd_port": 871, "nfs_srv_rpclockd_port": 32803, "nfs_srv_v4": false, "id": 1 }' -v >${RESTYOUT} 2>${RESTYERR}
+  check_rest_response "200 OK"
+
+  # Check creating a NFS share
+  echo_test_title "Creating a NFS share on /mnt/tank"
+  POST /sharing/nfs/ '{ "nfs_comment": "My Test Share", "nfs_paths": ["/mnt/tank"], "nfs_security": "sys" }' -v >${RESTYOUT} 2>${RESTYERR}
+  check_rest_response "201 CREATED"
+
+  # Now start the service
+  echo_test_title "Starting NFS service"
+  PUT /services/services/nfs/ '{ "srv_enable": true }' -v >${RESTYOUT} 2>${RESTYERR}
+  check_rest_response "200 OK"
+
+  # Now check if we can mount NFS / create / rename / copy / delete / umount
+  echo_test_title "Mounting NFS"
+  rc_halt "mkdir /tmp/nfs-mnt.$$"
+  rc_halt "mount_nfs ${ip}:/mnt/tank /tmp/nfs-mnt.$$" "umount /tmp/nfs-mnt.$$ ; rmdir /tmp/nfs-mnt.$$"
+
+  echo_test_title "Creating NFS file"
+  rc_halt "touch /tmp/nfs-mnt.$$/testfile" "umount /tmp/nfs-mnt.$$ ; rmdir /tmp/nfs-mnt.$$"
+
+  echo_test_title "Moving NFS file"
+  rc_halt "mv /tmp/nfs-mnt.$$/testfile /tmp/nfs-mnt.$$/testfile2"
+
+  echo_test_title "Copying NFS file"
+  rc_halt "cp /tmp/nfs-mnt.$$/testfile2 /tmp/nfs-mnt.$$/testfile"
+
+  echo_test_title "Deleting NFS file"
+  rc_halt "rm /tmp/nfs-mnt.$$/testfile2"
+
+  echo_test_title "Unmounting NFS"
+  rc_halt "umount /tmp/nfs-mnt.$$"
+  rc_halt "rmdir /tmp/nfs-mnt.$$"
+}
+
 if [ -z "$1" ] ; then
   # If we are running in jenkins mode, it make take a while for
   # FreeNAS to become available, depending upon node speed
@@ -87,51 +168,12 @@ if [ -z "$1" ] ; then
   done
 fi
 
-# Check getting disks
-echo "Checking for disks / API functionality"
-GET /storage/disk/ -v 2>${RESTYERR} >${RESTYOUT}
-check_rest_response "200 OK"
 
-# Check creating a zpool
-echo "Creating zpool tank"
-POST /storage/volume/ '{ "volume_name" : "tank", "layout" : [ { "vdevtype" : "stripe", "disks" : [ "ada1", "ada2" ] } ] }' -v >${RESTYOUT} 2>${RESTYERR}
-check_rest_response "201 CREATED"
+# Run the storage tests
+storage_tests
 
-# Check creating a dataset
-echo "Creating dataset tank/share"
-POST /storage/volume/1/datasets/ '{ "name": "share" }' -v >${RESTYOUT} 2>${RESTYERR}
-check_rest_response "201 CREATED"
-
-# Set the permissions of the dataset
-echo "Setting permissions of /mnt/tank"
-PUT /storage/permission/ '{ "mp_path": "/mnt/tank", "mp_acl": "unix", "mp_mode": "777", "mp_user": "root", "mp_group": "wheel" }' -v >${RESTYOUT} 2>${RESTYERR}
-check_rest_response "201 CREATED"
-
-# Enable NFS server
-echo "Creating the NFS server"
-PUT /services/nfs/ '{ "nfs_srv_bindip": "'"${ip}"'", "nfs_srv_mountd_port": 618, "nfs_srv_allow_nonroot": false, "nfs_srv_servers": 10, "nfs_srv_udp": false, "nfs_srv_rpcstatd_port": 871, "nfs_srv_rpclockd_port": 32803, "nfs_srv_v4": false, "id": 1 }' -v >${RESTYOUT} 2>${RESTYERR}
-check_rest_response "200 OK"
-
-# Check creating a NFS share
-echo "Creating a NFS share on /mnt/tank"
-POST /sharing/nfs/ '{ "nfs_comment": "My Test Share", "nfs_paths": ["/mnt/tank"], "nfs_security": "sys" }' -v >${RESTYOUT} 2>${RESTYERR}
-check_rest_response "201 CREATED"
-
-# Now start the service
-echo "Starting NFS service"
-PUT /services/services/nfs/ '{ "srv_enable": true }' -v >${RESTYOUT} 2>${RESTYERR}
-check_rest_response "200 OK"
-
-# Now check if we can mount NFS / create / rename / copy / delete / umount
-echo "Checking NFS mount"
-rc_halt "mkdir /tmp/nfs-mnt.$$"
-rc_halt "mount_nfs ${ip}:/mnt/tank /tmp/nfs-mnt.$$" "umount /tmp/nfs-mnt.$$ ; rmdir /tmp/nfs-mnt.$$"
-rc_halt "touch /tmp/nfs-mnt.$$/testfile" "umount /tmp/nfs-mnt.$$ ; rmdir /tmp/nfs-mnt.$$"
-rc_halt "mv /tmp/nfs-mnt.$$/testfile /tmp/nfs-mnt.$$/testfile2"
-rc_halt "cp /tmp/nfs-mnt.$$/testfile2 /tmp/nfs-mnt.$$/testfile"
-rc_halt "rm /tmp/nfs-mnt.$$/testfile2"
-rc_halt "umount /tmp/nfs-mnt.$$"
-rc_halt "rmdir /tmp/nfs-mnt.$$"
+# Run the NFS tests
+nfs_tests
 
 # Made it to the end, exit with success!
 exit 0
