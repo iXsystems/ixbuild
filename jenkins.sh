5c9067eb6d2f54092e5f8d5af36ff2cd5cbaa0bc
Kris Moore <kris@pcbsd.org>
2015-07-08 22:11:55 -0400

    Don't need to look for WORKSPACE on command line

--- jenkins.sh
+++ jenkins.sh
@@ -14,15 +14,6 @@ cd `dirname $mypath`
 . functions.sh
 ######################################################
 
-# Check for the WORKSPACE tag
-for var in $@
-do
- echo $var | grep -q "WORKSPACE="
- if [ $? -eq 0 ] ; then
-   export WORKSPACE=`echo $var | cut -d '=' -f 2`
- fi
-done
-
 case $TYPE in
   world) jenkins_world ;;
    jail) jenkins_jail ;;
