ee0adf4ba0c8cf424cbd9a4ac1406a0d11002d4e
Kris Moore <kris@pcbsd.org>
2016-01-07 11:16:29 -0500

    Add a new NodeJS utility we will eventually use for doing automated testing of WebSockets in addition to REST. It does the WS connection / auth, and then sends over a payload and returns with  the response, for another script to parse to determine result.

---
+++ utils/sendwebsocket.js
@@ -0,0 +1,57 @@
+var WebSocket = require('ws');
+var wsserver = process.argv[2];
+var stdargs = "";
+var ignorefirst = "true";
+
+function connectWebSocket()
+{
+  websocket = new WebSocket(wsserver);
+  websocket.onopen = function(evt) { onOpen(evt) };
+  websocket.onclose = function(evt) { onClose(evt) };
+  websocket.onmessage = function(evt) { onMessage(evt) };
+  websocket.onerror = function(evt) { onError(evt) };
+}
+
+function onOpen(evt)
+{
+  var authjson = '{ "namespace":"rpc", "name":"auth", "id":"authrequest", "args": { "username":"' + process.argv[3] +'", "password":"' + process.argv[4] + '" } }';
+  nstatus = "auth";
+  wstatus = "idle";
+  doSend(authjson);
+  doSend(stdargs);
+  websocket.close();
+}
+
+function onClose(evt)
+{
+}
+
+function onMessage(evt)
+{
+  var jsonobj = JSON.parse(evt.data);
+  if ( ignorefirst == "true" ) {
+    ignorefirst = "false";
+  } else {
+    console.log(JSON.stringify(jsonobj, null, 2));
+  }
+}
+
+function doSend(message)
+{
+  //console.log('Sent: ' + message);
+  websocket.send(message);
+}
+
+function readStdIn(msg)
+{
+  stdargs = msg;
+}
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function(message) {
+  readStdIn(message);
+});
+
+connectWebSocket();
