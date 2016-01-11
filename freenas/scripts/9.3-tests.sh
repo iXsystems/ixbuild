98d80eb09bdb5ff0abae03b561f541ce930fc2f9
Kris Moore <kris@pcbsd.org>
2016-01-11 15:37:37 -0500

    Large change to testing framework, convert the specific tests into modules, which are read by the testing script on the fly and run.
    These specific modules can list REQUIRES, which will make them
    only run after the specific other tests have executed. I.E. you
    cant create a NFS share until the storage tests have run, creating
    a zpool.
    
    This will let us next add targeted testing, I.E. you want to only
    run NFS regression testing by hand.
    
    Also, add thee types of tests here, SMOKE/COMPLETE/BENCHMARK. Right now
    all tests are SMOKE tests, but we will begin creating more in-depth tests
    which are run in COMPLETE mode, and eventually benchmark tests as well,
    which we need to be able to run on the fly.

--- freenas/scripts/9.3-tests.sh
+++ freenas/scripts/9.3-tests.sh
@@ -35,6 +35,9 @@ fi
 # Set variable to call jsawk utility
 JSAWK="${PROGDIR}/../utils/jsawk -j js24"
 
+# Test Module directory
+TDIR="${PROGDIR}/9.3-tests"
+
 # Log files
 RESTYOUT=/tmp/resty.out
 RESTYERR=/tmp/resty.err
@@ -44,6 +47,10 @@ TOTALCOUNT="0"
 #################################################################
 # Run the tests now!
 #################################################################
+
+# Set the default test type
+TESTSET="SMOKE"
+export TESTSET
 
 echo_ok()
 {
@@ -76,7 +83,7 @@ rc_halt()
     ${1} >/tmp/.cmdTest.$$ 2>/.cmdTest.$$
     if [ $? -ne 0 ] ; then
        echo -e " - FAILED"
-       ${2}
+       eval "${2}"
        echo "Failed running: $1"
        cat /tmp/.cmdTest.$$
        rm /tmp/.cmdTest.$$
@@ -117,7 +124,7 @@ rc_halt()
   kill -9 $(cat /tmp/.rc-pid.$$)
   rm /tmp/.rc-pid.$$
   echo -e " - FAILED"
-  ${2}
+  eval "${2}"
   echo "Timeout running: $1"
   cat /tmp/.cmdTest.$$
   rm /tmp/.cmdTest.$$
@@ -137,165 +144,6 @@ echo_test_title()
   TOTALCOUNT=`expr $TOTALCOUNT + 1`
   sync
   echo -e "Running $GROUPTEXT ($TCOUNT/$TOTALTESTS) - $1\c"
-}
-
-storage_tests()
-{
-  # Set the group text and number of tests
-  set_test_group_text "Storage tests" "6"
-
-  # Check getting disks
-  echo_test_title "Disks / API functionality"
-  GET /storage/disk/ -v 2>${RESTYERR} >${RESTYOUT}
-  check_rest_response "200 OK"
-  echo_ok
-
-  # Check creating a zpool
-  echo_test_title "Creating volume"
-  POST /storage/volume/ '{ "volume_name" : "tank", "layout" : [ { "vdevtype" : "stripe", "disks" : [ "ada1", "ada2" ] } ] }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "201 CREATED"
-  echo_ok
-
-  # Check creating a dataset /share
-  echo_test_title "Creating dataset tank/share"
-  POST /storage/volume/tank/datasets/ '{ "name": "share" }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "201 CREATED"
-  echo_ok
-
-  # Check creating a dataset /jails
-  echo_test_title "Creating dataset tank/jails"
-  POST /storage/volume/tank/datasets/ '{ "name": "jails" }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "201 CREATED"
-  echo_ok
-
-  # Set the permissions of the dataset
-  echo_test_title "Changing permissions on /mnt/tank"
-  PUT /storage/permission/ '{ "mp_path": "/mnt/tank", "mp_acl": "unix", "mp_mode": "777", "mp_user": "root", "mp_group": "wheel" }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "201 CREATED"
-  echo_ok
-
-  # Set the permissions of the dataset
-  echo_test_title "Changing permissions on /mnt/tank/share"
-  PUT /storage/permission/ '{ "mp_path": "/mnt/tank/share", "mp_acl": "unix", "mp_mode": "777", "mp_user": "root", "mp_group": "wheel" }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "201 CREATED"
-  echo_ok
-}
-
-nfs_tests()
-{
-  # Set the group text and number of tests
-  set_test_group_text "NFS tests" "9"
-
-  # Enable NFS server
-  echo_test_title "Creating the NFS server"
-  #PUT /services/nfs/ '{ "nfs_srv_bindip": "'"${ip}"'", "nfs_srv_mountd_port": 618, "nfs_srv_allow_nonroot": false, "nfs_srv_servers": 10, "nfs_srv_udp": false, "nfs_srv_rpcstatd_port": 871, "nfs_srv_rpclockd_port": 32803, "nfs_srv_v4": true, "nfs_srv_v4_krb": true, "id": 1 }' -v >${RESTYOUT} 2>${RESTYERR}
-  PUT /services/nfs/ '{ "nfs_srv_bindip": "'"${ip}"'", "nfs_srv_mountd_port": 618, "nfs_srv_allow_nonroot": false, "nfs_srv_servers": 10, "nfs_srv_udp": false, "nfs_srv_rpcstatd_port": 871, "nfs_srv_rpclockd_port": 32803, "nfs_srv_v4": false, "nfs_srv_v4_krb": false, "id": 1 }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "200 OK"
-  echo_ok
-
-  # Check creating a NFS share
-  echo_test_title "Creating a NFS share on /mnt/tank/share"
-  POST /sharing/nfs/ '{ "nfs_comment": "My Test Share", "nfs_paths": ["/mnt/tank/share"], "nfs_security": "sys" }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "201 CREATED"
-  echo_ok
-
-  # Now start the service
-  echo_test_title "Starting NFS service"
-  PUT /services/services/nfs/ '{ "srv_enable": true }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "200 OK"
-  echo_ok
-
-  # Now check if we can mount NFS / create / rename / copy / delete / umount
-  echo_test_title "Mounting NFS"
-  rc_halt "mkdir /tmp/nfs-mnt.$$"
-  rc_halt "mount_nfs ${ip}:/mnt/tank/share /tmp/nfs-mnt.$$" "umount /tmp/nfs-mnt.$$ ; rmdir /tmp/nfs-mnt.$$" "60"
-  echo_ok
-
-  echo_test_title "Creating NFS file"
-  rc_halt "touch /tmp/nfs-mnt.$$/testfile" "umount /tmp/nfs-mnt.$$ ; rmdir /tmp/nfs-mnt.$$"
-  echo_ok
-
-  echo_test_title "Moving NFS file"
-  rc_halt "mv /tmp/nfs-mnt.$$/testfile /tmp/nfs-mnt.$$/testfile2"
-  echo_ok
-
-  echo_test_title "Copying NFS file"
-  rc_halt "cp /tmp/nfs-mnt.$$/testfile2 /tmp/nfs-mnt.$$/testfile"
-  echo_ok
-
-  echo_test_title "Deleting NFS file"
-  rc_halt "rm /tmp/nfs-mnt.$$/testfile2"
-  echo_ok
-
-  echo_test_title "Unmounting NFS"
-  rc_halt "umount /tmp/nfs-mnt.$$"
-  rc_halt "rmdir /tmp/nfs-mnt.$$"
-  echo_ok
-}
-
-iscsi_tests()
-{
-  # Set the group text and number of tests
-  set_test_group_text "iSCSI tests" "1"
-
-  # Enable iSCSI server
-  echo_test_title "Creating iSCSI extent"
-  POST /services/iscsi/extent/ '{ "iscsi_target_extent_type": "File", "iscsi_target_extent_name": "extent", "iscsi_target_extent_filesize": "50MB", "iscsi_target_extent_path": "/mnt/tank/iscsi" }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "201 CREATED"
-  echo_ok
-
-}
-
-cifs_tests()
-{
-  # Set the group text and number of tests
-  set_test_group_text "CIFS tests" "9"
-
-  echo_test_title "Enabling CIFS service"
-  PUT /services/cifs/ '{ "cifs_srv_description": "Test FreeNAS Server", "cifs_srv_guest": "nobody", "cifs_hostname_lookup": false, "cifs_srv_aio_enable": false, "cifs_srv_netbiosname": "testnas" }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "200 OK"
-  echo_ok
-
-  echo_test_title "Creating a CIFS share on /mnt/tank/share"
-  POST /sharing/cifs/ '{ "cfs_comment": "My Test CIFS Share", "cifs_path": "/mnt/tank/share", "cifs_name": "TestShare", "cifs_guestok": true, "cifs_vfsobjects": "streams_xattr" }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "201 CREATED"
-  echo_ok
-
-  # Now start the service
-  echo_test_title "Starting CIFS service"
-  PUT /services/services/cifs/ '{ "srv_enable": true }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "200 OK"
-  echo_ok
-
-  sleep 3
-
-  # Now check if we can mount NFS / create / rename / copy / delete / umount
-  echo_test_title "Mounting CIFS"
-  rc_halt "mkdir /tmp/cifs-mnt.$$"
-  sync
-  rc_halt "mount_smbfs -N -I ${ip} //guest@testnas/TestShare /tmp/cifs-mnt.$$" "rmdir /tmp/cifs-mnt.$$"
-  echo_ok
-
-  echo_test_title "Creating CIFS file"
-  rc_halt "touch /tmp/cifs-mnt.$$/testfile" "umount /tmp/cifs-mnt.$$ ; rmdir /tmp/cifs-mnt.$$"
-  echo_ok
-
-  echo_test_title "Moving CIFS file"
-  rc_halt "mv /tmp/cifs-mnt.$$/testfile /tmp/cifs-mnt.$$/testfile2"
-  echo_ok
-
-  echo_test_title "Copying CIFS file"
-  rc_halt "cp /tmp/cifs-mnt.$$/testfile2 /tmp/cifs-mnt.$$/testfile"
-  echo_ok
-
-  echo_test_title "Deleting CIFS file"
-  rc_halt "rm /tmp/cifs-mnt.$$/testfile2"
-  echo_ok
-
-  echo_test_title "Unmounting CIFS"
-  rc_halt "umount /tmp/cifs-mnt.$$"
-  rc_halt "rmdir /tmp/cifs-mnt.$$"
-  echo_ok
 }
 
 # Set the IP address of REST
@@ -326,156 +174,6 @@ set_ip()
   echo_ok
 }
 
-# Run a series of tests on the boot-environments
-bootenv_tests() {
-  set_test_group_text "Boot-Environment Tests" "3"
-
-  echo_test_title "Creating a new boot-environment: newbe1"
-  POST /system/bootenv/ '{ "name": "newbe1", "source": "default" }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "201 CREATED"
-  echo_ok
-
-  echo_test_title "Creating a new boot-environment: newbe2"
-  POST /system/bootenv/ '{ "name": "newbe2", "source": "newbe1" }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "201 CREATED"
-  echo_ok
-
-  echo_test_title "Removing a boot-environment: newbe2"
-  DELETE /system/bootenv/newbe2 '' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "204 NO CONTENT"
-  echo_ok
-}
-
-# Run a series of tests on the ssh settings
-# We also leave it enabled, so we have access to VM if something hangs / goes wrong
-ssh_tests() {
-  set_test_group_text "SSH Tests" "2"
-
-  echo_test_title "Configuring SSH settings"
-  PUT /services/ssh/ '{ "ssh_rootlogin": true }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "200 OK"
-  echo_ok
-
-  echo_test_title "Enabling SSH Service"
-  PUT /services/services/ssh/ '{ "srv_enable": true }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "200 OK"
-  echo_ok
-
-}
-
-# Run a series of tests on the email settings
-email_tests() {
-  set_test_group_text "E-Mail Tests" "1"
-
-  echo_test_title "Configuring e-mail settings"
-  PUT /system/email/ '{ "em_fromemail": "william.spam@ixsystems.com", "em_outgoingserver": "mail.ixsystems.com", "em_pass": "changeme", "em_port": 25, "em_security": "plain", "em_smtp": true, "em_user": "william.spam@ixsystems.com" }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "200 OK"
-  echo_ok
-}
-
-# Run a series of tests on jail creation
-jail_tests() {
-  set_test_group_text "Jail Tests" "6"
-
-  echo_test_title "Configuring jails"
-  PUT /jails/configuration/ '{ "jc_ipv4_network_start": "192.168.56.150", "jc_path": "/mnt/tank/jails" }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "201 CREATED"
-  echo_ok
-
-  echo_test_title "Creating jail - VNET OFF"
-  POST /jails/jails/ '{ "jail_host": "testjail", "jail_defaultrouter_ipv4": "192.168.56.1", "jail_ipv4": "192.168.56.155", "jail_ipv4_netmask": "24", "jail_vnet": false }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "201 CREATED"
-  echo_ok
-
-  echo_test_title "Mount tank/share into jail"
-  POST /jails/mountpoints/ '{ "destination": "/mnt", "jail": "testjail", "mounted": true, "readonly": false, "source": "/mnt/tank/share" }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "201 CREATED"
-  echo_ok
-
-  echo_test_title "Starting jail"
-  POST /jails/jails/1/start/ '' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "202 ACCEPTED"
-  echo_ok
-
-  echo_test_title "Restarting jail"
-  POST /jails/jails/1/restart/ '' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "202 ACCEPTED"
-  echo_ok
-
-  echo_test_title "Stopping jail"
-  POST /jails/jails/1/stop/ '' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "202 ACCEPTED"
-  echo_ok
-}
-
-rsyncd_tests() {
-  set_test_group_text "RSYNC tests" "4"
-
-  echo_test_title "Configuring rsyncd service"
-  PUT /services/rsyncd/ '{ "rsyncd_port": 873 }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "200 OK"
-  echo_ok
-
-  echo_test_title "Starting rsyncd service"
-  PUT /services/services/rsync/ '{ "srv_enable": true }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "200 OK"
-  echo_ok
-
-  echo_test_title "Creating rsync resource"
-  POST /services/rsyncmod/ '{ "rsyncmod_name": "testmod", "rsyncmod_path": "/mnt/tank/share" }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "201 CREATED"
-  echo_ok
-
-  # Test rsync
-  echo_test_title "Testing rsync access"
-  rc_halt "rsync -avn ${ip}::testmod"
-  echo_ok
-}
-
-ftp_tests() {
-  set_test_group_text "FTP tests" "3"
-
-  echo_test_title "Configuring ftp service"
-  PUT /services/ftp/ '{ "ftp_clients": 10, "ftp_rootlogin": true }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "200 OK"
-  echo_ok
-
-  echo_test_title "Starting ftp service"
-  PUT /services/services/ftp/ '{ "srv_enable": true }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "200 OK"
-  echo_ok
-
-  echo_test_title "Fetching file via FTP"
-  rc_halt "ftp -o /tmp/ftpfile ftp://testuser:test@${ip}/.cshrc"
-  echo_ok
-}
-
-user_tests() {
-  set_test_group_text "User tests" "3"
-
-  # Create dataset for the home directory
-  echo_test_title "Creating home dataset tank/testuser"
-  POST /storage/volume/tank/datasets/ '{ "name": "testuser" }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "201 CREATED"
-  echo_ok
-
-  # Create the testuser
-  echo_test_title "Creating user: testuser"
-  POST /account/users/ '{ "bsdusr_username": "testuser", "bsdusr_creategroup": true, "bsdusr_full_name": "Test User", "bsdusr_password": "test", "bsdusr_uid": 1111, "bsdusr_home": "/mnt/tank/testuser", "bsdusr_mode": "755", "bsdusr_shell": "/bin/csh" }' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "201 CREATED"
-  echo_ok
-
-  # Get the ID of this user
-  userid=$(cat ${RESTYOUT} | ${JSAWK} 'return this.id')
-
-  # Set the user to part of the 'wheel' / 'ftp' groups
-  echo_test_title "Setting user groups: wheel,ftp"
-  POST /account/users/${userid}/groups/ '["wheel","ftp"]' -v >${RESTYOUT} 2>${RESTYERR}
-  check_rest_response "202 OK"
-  echo_ok
-
-}
-
 wait_for_avail()
 {
   count=0
@@ -494,6 +192,69 @@ wait_for_avail()
   done
 }
 
+run_module() {
+  unset REQUIRES
+
+  # Source the module now
+  cd ${TDIR}
+  . ./${1}
+  if [ $? -ne 0 ] ; then
+    echo "Failed sourcing ${1}"
+    exit 1
+  fi
+
+  # Make sure any required module have been run first
+  local modreq="$REQUIRES"
+  if [ -n "$modreq" ] ; then
+    for i in $modreq
+    do
+      # Check if this module has already been run
+      echo $RUNMODULES | grep -q ":::${i}:::"
+      if [ $? -eq 0 ] ; then continue; fi
+
+      # Need to run another module first
+      echo "***** Running module dependancy: $i *****"
+      run_module "$i" "quiet"
+    done
+  fi
+
+  # Run the target module
+  if [ -z "$2" ] ; then
+    echo "***** Running module: $1 *****"
+  fi
+  eval "${1}_init"
+  if [ $? -ne 0 ] ; then
+    echo "Failed on test module: $1"
+    exit 1
+  fi
+
+  # Save that this test was already run
+  RUNMODULES="${RUNMODULES}:::${1}:::"
+}
+
+# Read through the test modules and start running them
+read_module_dir() {
+  cd ${TDIR}
+  if [ $? -ne 0 ] ; then
+    echo "Missing test module dir"
+    exit 1
+  fi
+
+  RUNMODULES=""
+
+  for module in `ls`
+  do
+    # Skip the README, other files should be valid though
+    if [ "$module" = "README" ] ; then continue ; fi
+
+    # Check if this module has already been run
+    echo $RUNMODULES | grep -q ":::${module}:::"
+    if [ $? -eq 0 ] ; then continue ; fi
+
+    run_module "$module"
+  done
+}
+
 # When running via Jenkins / ATF mode, it may take a variable
 # time to boot the system and be ready for REST calls. We run
 # an initial test to determine when the interface is up
@@ -504,38 +265,8 @@ echo_ok
 # Reset the IP address via REST
 set_ip
 
-# Setup SSH access
-ssh_tests
-
-# Check e-mail configuration
-email_tests
-
-# Check boot environment support
-bootenv_tests
-
-# Run the storage tests
-storage_tests
-
-# Run the user tests
-user_tests
-
-# Run the rsyncd tests
-rsyncd_tests
-
-# Run the CIFS tests
-cifs_tests
-
-# Run the FTP tests
-ftp_tests
-
-# Run the NFS tests
-nfs_tests
-
-# Run the iSCSI tests
-iscsi_tests
-
-# Run the jail tests
-jail_tests
+# Now start going through our test modules
+read_module_dir
 
 # Made it to the end, exit with success!
 echo "SUCCESS - $TOTALCOUNT tests run - REST API testing complete!"
