efd31a699408e4ea7960fa7ae037a33c4d74dd4b
Kris Moore <kris@pcbsd.org>
2016-04-28 14:28:19 -0400

    Start adding the glue to do a live upgrade of a FreeNAS box

--- freenas/scripts/3.runlivetests.sh
+++ freenas/scripts/3.runlivetests.sh
@@ -1,42 +1,57 @@
-#!/bin/sh
+#!/usr/local/bin/bash
 
 # Where is the pcbsd-build program installed
 PROGDIR="`realpath | sed 's|/scripts$||g'`" ; export PROGDIR
 
-# Source our functions
-. ${PROGDIR}/scripts/functions.sh
-
 # Make sure we have all the required packages installed
 ${PROGDIR}/scripts/checkprogs.sh
+
+# Source our functions
+. ${PROGDIR}/scripts/functions.sh
+. ${PROGDIR}/scripts/functions-tests.sh
 
 if [ ! -d "${PROGDIR}/tmp" ] ; then
   mkdir ${PROGDIR}/tmp
 fi
 
-
-# Set the default build directory
-FNASBDIR="/freenas"
-export FNASBDIR
-
-# Figure out the upgrade name
-echo "Finding Update directory..."
-if [ -d "${FNASBDIR}/_BE/release" ] ; then
-  UPMANI=`find ${FNASBDIR}/_BE/release | grep FreeNAS-MANIFEST`
-  UPDIR="$(dirname $UPMANI)"/Packages
-fi
-
-# If no upgrade found
-if [ -z "$UPDIR" -o ! -d "$UPDIR" ] ; then
-  exit_err "Failed locating upgrade files, did 'make release' work?"
-fi
-
-# Is this TrueNAS or FreeNAS?
-echo $UPDIR | grep -q "TrueNAS"
+# Figure out the flavor for this test
+echo $BUILDTAG | grep -q "truenas"
 if [ $? -eq 0 ] ; then
-   export FLAVOR="TRUENAS"
+  FLAVOR="TRUENAS"
 else
-   export FLAVOR="FREENAS"
+  FLAVOR="FREENAS"
 fi
 
-echo "Using Upgrade directory: $UPDIR"
+# Source our resty / jsawk functions
+. ${PROGDIR}/../utils/resty -W "http://${LIVEHOST}:80/api/v1.0" -H "Accept: application/json" -H "Content-Type: application/json" -u ${LIVEUSER}:${LIVEPASS}
 
+# Start the XML reporting
+start_xml_results "Live Testing"
+
+# Check that the server is up and ready to answer calls
+set_test_group_text "Testing Connectivity" "1"
+echo_test_title "Testing access to REST API"
+wait_for_avail
+echo_ok
+
+if [ "$FLAVOR" = "FREENAS" ] ; then
+  set_test_group_text "Upgrade Test" "2"
+
+  # Do the update now
+  echo_test_title "Performing upgrade of system"
+  rest_request "POST" "/system/update/update/" "''"
+  check_rest_response "200 OK"
+
+  # Wait for system to reboot
+  echo_test_title "Waiting for reboot"
+  sleep 20
+  wait_for_avail
+  echo_ok
+
+else
+  # For TrueNAS we have to do the update in two stages, one for each head
+
+fi
+
+
+finish_xml_results
