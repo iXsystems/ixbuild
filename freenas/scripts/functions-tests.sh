6ddb8f8d4068bafcc7cdcaaca27504a42cd5bd43
Joe Maloney <jmaloney@joe-desktop.tn.ixsystems.com>
2016-07-18 17:00:21 -0400

    Also copy logs into it's own directory

--- freenas/scripts/functions-tests.sh
+++ freenas/scripts/functions-tests.sh
@@ -118,14 +118,15 @@ save_artifacts_on_fail()
             chown jenkins:jenkins "${WORKSPACE}/artifacts"
               if [ ! -d "${WORKSPACE}/artifacts/${FNASBDIR}" ] ; then
                 mkdir "${WORKSPACE}/artifacts/${FNASBDIR}"
+		mkdir "${WORKSPACE}/artifacts/${FNASBDIR}logs"
 		mkdir "${WORKSPACE}/artifacts/${FNASBDIR}/ports"
                 chown jenkins:jenkins "${WORKSPACE}/artifacts/${FNASBDIR}"
         fi
       fi
     fi
     echo "Saving artifacts to: ${WORKSPACE}/artifacts${FNASBDIR}"
-    cp -R "${FNASBDIR}/_BE/objs/logs" "${WORKSPACE}/artifacts/${FNASBDIR}"
-    cp -R "${FNASBDIR}/_BE/objs/ports/logs" "${WORKSPACE}/artifacts/${FNASBDIR}/ports/"
+    cp -R "${FNASBDIR}/_BE/objs/logs/" "${WORKSPACE}/artifacts/${FNASBDIR}/logs/"
+    cp -R "${FNASBDIR}/_BE/objs/ports/logs/" "${WORKSPACE}/artifacts/${FNASBDIR}/ports/"
     chown -R jenkins:jenkins "${WORKSPACE}/artifacts/${FNASBDIR}"
   else
     echo "Skip saving artificats on failure / ARTIFACTONFAIL not set"
@@ -142,14 +143,15 @@ save_artifacts_on_success()
         chown jenkins:jenkins "${WORKSPACE}/artifacts"
           if [ ! -d "${WORKSPACE}/artifacts/${FNASBDIR}" ] ; then
             mkdir "${WORKSPACE}/artifacts/${FNASBDIR}"
-	    mkdir "${WORKSPACE}/artifacts/${FNASBDIR}/ports/"
+	    mkdir "${WORKSPACE}/artifacts/${FNASBDIR}/logs"
+	    mkdir "${WORKSPACE}/artifacts/${FNASBDIR}/ports"
             chown jenkins:jenkins "${WORKSPACE}/artifacts/${FNASBDIR}"
         fi
       fi
     fi
     echo "Saving artifacts to: ${WORKSPACE}/artifacts${FNASBDIR}"
-    cp -R "${FNASBDIR}/_BE/objs/logs" "${WORKSPACE}/artifacts/${FNASBDIR}"
-    cp -R "${FNASBDIR}/_BE/objs/ports/logs" "${WORKSPACE}/artifacts/${FNASBDIR}/ports/"
+    cp -R "${FNASBDIR}/_BE/objs/logs/" "${WORKSPACE}/artifacts/${FNASBDIR}/logs/"
+    cp -R "${FNASBDIR}/_BE/objs/ports/logs/" "${WORKSPACE}/artifacts/${FNASBDIR}/ports/"
     chown -R jenkins:jenkins "${WORKSPACE}/artifacts/${FNASBDIR}"
   else
     echo "Skip saving artificats on success / ARTIFACTONSUCCESS not set"
