ac9d92e87f0e335fdb93e2cd6cb3339ba6fe23a7
Kris Moore <kris@pcbsd.org>
2016-07-22 16:15:57 -0400

    Fix location of API upload

--- functions.sh
+++ functions.sh
@@ -415,7 +415,7 @@ jenkins_freenas_push_api()
     if [ $? -ne 0 ] ; then exit_clean ; fi
 
     # Make them live!
-    rsync -a -v -z --delete --exclude "truenas*" -e 'ssh -i /root/.ssh/id_dsa.jenkins' . jenkins@api.freenas.org:/tank/api/html
+    rsync -a -v -z --delete --exclude "truenas*" -e 'ssh -i /root/.ssh/id_dsa.jenkins' /tmp/apipush jenkins@api.freenas.org:/tank/api/html
     if [ $? -ne 0 ] ; then exit_clean; fi
 
     rm -rf /tmp/apipush 2>/dev/null
