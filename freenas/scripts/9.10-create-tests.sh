5ae2e73a3a640e53b53c5e0068ce645b8ad63e29
Joe Maloney <pkgdemonteam@gmail.com>
2016-07-27 09:22:47 -0400

    Always finish xml results

--- freenas/scripts/9.10-create-tests.sh
+++ freenas/scripts/9.10-create-tests.sh
@@ -105,6 +105,6 @@ fi
 # Made it to the end, exit with success!
 #echo "$RESULT - $TOTALCOUNT tests run - REST API testing complete!"
 
-#finish_xml_results "$TOTALCOUNT"
+finish_xml_results "$TOTALCOUNT"
 
 exit 0
