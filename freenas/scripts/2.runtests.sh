324b003578e59005ca51917e214845309d2487d0
Joe Maloney <pkgdemonteam@gmail.com>
2016-09-12 10:48:49 -0400

    Move this back to runtests.  Not worth the effort to reuse elsewhere.

--- freenas/scripts/2.runtests.sh
+++ freenas/scripts/2.runtests.sh
@@ -11,8 +11,57 @@
 # Make sure we have all the required packages installed
 ${PROGDIR}/scripts/checkprogs.sh
 
-# Create ISO for VM
-create_auto_install
+# Prepare to build autoinstall ISO
+if [ ! -d "${PROGDIR}/tmp" ] ; then
+  mkdir ${PROGDIR}/tmp
+fi
+# Set local location of FreeNAS build
+if [ -n "$BUILDTAG" ] ; then
+  FNASBDIR="/$BUILDTAG"
+else
+  FNASBDIR="/freenas"
+fi
+export FNASBDIR
+
+# Set the name for VM
+VM="$BUILDTAG"
+export VM
+
+# Figure out the ISO name
+echo "Finding ISO file..."
+if [ -d "${FNASBDIR}/objs" ] ; then
+  ISOFILE=`find ${FNASBDIR}/objs | grep '\.iso$' | head -n 1`
+elif [ -d "${FNASBDIR}/_BE/release" ] ; then
+  ISOFILE=`find ${FNASBDIR}/_BE/release | grep '\.iso$' | head -n 1`
+else
+  if [ -n "$1" ] ; then
+    ISOFILE=`find ${1} | grep '\.iso$' | head -n 1`
+  else
+    ISOFILE=`find ${PROGDIR}/../objs | grep '\.iso$' | head -n 1`
+  fi
+fi
+
+# If no ISO found
+if [ -z "$ISOFILE" ] ; then
+  exit_err "Failed locating ISO file, did 'make release' work?"
+fi
+
+# Is this TrueNAS or FreeNAS?
+echo $ISOFILE | grep -q "TrueNAS"
+if [ $? -eq 0 ] ; then
+   export FLAVOR="TRUENAS"
+else
+   export FLAVOR="FREENAS"
+fi
+
+echo "Using ISO: $ISOFILE"
+
+# Create the automatic ISO installer
+cd ${PROGDIR}/tmp
+${PROGDIR}/scripts/create-auto-install.sh ${ISOFILE}
+if [ $? -ne 0 ] ; then
+  exit_err "Failed creating auto-install ISO!"
+fi
 
 # Determine which VM backend to start
 if [ -n "$USE_BHYVE" ] ; then
