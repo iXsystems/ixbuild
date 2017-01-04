76396c85afd8a1f78e374ce9fe0cd021d60ec13c
Joe Maloney <pkgdemonteam@gmail.com>
2017-01-04 15:39:50 -0500

    Fix target.conf modification

--- freenas/scripts/api-v2.0-tests.sh
+++ freenas/scripts/api-v2.0-tests.sh
@@ -14,11 +14,11 @@
 echo "Using API Address: ${ip}/v2.0"
 
 cd $FNASSRC/src/middlewared/middlewared/pytest
-echo [Target] >> target.conf
-echo uri = http://10.20.0.130 > target.conf
-echo api = /api/v2.0/ > target.conf
-echo username = "root" > target.conf
-echo password = "testing" > target.conf
+echo [Target] > target.conf
+echo uri = http://10.20.0.130 >> target.conf
+echo api = /api/v2.0/ >> target.conf
+echo username = "root" >> target.conf
+echo password = "testing" >> target.conf
 sed -i '' "s|'freenas'|'testing'|g" functional/test_0001_authentication.py
 py.test -sv functional --junitxml=$RESULTSDIR/api-v2.0-results.xml
 
