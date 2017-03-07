cce5856d9c9043cf1addad5867ff4025023aa2f9
Joe Maloney <jmaloney@joe-laptop.tn.ixsystems.com>
2017-03-06 19:03:13 -0500

    Use vmware-cmd to interact with VMs

--- freenas/scripts/2.runtests.sh
+++ freenas/scripts/2.runtests.sh
@@ -83,6 +83,10 @@ case ${VMBACKEND} in
      daemon -p /tmp/vmcu.pid cu -l /dev/ttyu0 -s 115200 > /tmp/console.log 2>/dev/null &
      sleep 30
            clean_xml_results
+	   revert_vmware
+	   stop_vmware
+	   start_vmware
+	   start_vmware
            exit 0
      ;;
   *) start_vbox ;;
