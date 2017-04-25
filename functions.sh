f87c5590ce4e9e60cbb050dc22c7165fbd0f3d3e
Joe Maloney <jmaloney@Joes-iMac.local>
2017-04-25 15:40:44 -0400

    Fix home dir path for jails

--- functions.sh
+++ functions.sh
@@ -1152,7 +1152,7 @@ jenkins_freenas_tests_jailed()
   iocage destroy -f $BUILDTAG 2>/dev/null
   iocage create -b tag=$BUILDTAG -t executor && \
   echo "/mnt/tank/autoinstalls /mnt/tank/iocage/tags/$BUILDTAG/root/autoinstalls nullfs rw 0 0" >> /mnt/tank/iocage/tags/$BUILDTAG/fstab && \
-  echo "/mnt/tank/home/jenkins /mnt/tank/iocage/tags/$BUILDTAG/root/mnt/home/home/jenkins nullfs rw 0 0" >> /mnt/tank/iocage/tags/$BUILDTAG/fstab && \
+  echo "/mnt/tank/home/jenkins /mnt/tank/iocage/tags/$BUILDTAG/root/mnt/home/jenkins nullfs rw 0 0" >> /mnt/tank/iocage/tags/$BUILDTAG/fstab && \
   echo "/mnt/tank/ixbuild /mnt/tank/iocage/tags/$BUILDTAG/root/mnt/tank/ixbuild nullfw rw 0 0" >> /mnt/tank/iocage/tags/$BUILDTAG/fstab && \
   iocage start $BUILDTAG
 }
