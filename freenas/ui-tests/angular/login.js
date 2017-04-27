d862dc989bdec2ed3c80c3994d7bfcc3a089bcb2
christiand992 <christian@ixsystems.com>
2017-04-27 16:41:23 -0400

    Issues with protractor detecting angular app - hacks to fix
    * Remove use of 'by.model' element selector due to angular not being 'detected'
    * Adds browser.ignoreSynchronization=true to get around angular timeout
    * Adds --rootElement=app to unsuccessfully detect angular app

--- freenas/ui-tests/angular/login.js
+++ freenas/ui-tests/angular/login.js
@@ -1,12 +1,16 @@
 describe('angularjs login', function() {
   it('should authenticate default user', function() {
-    browser.get('#/login');
+    // http://stackoverflow.com/questions/36201691/protractor-angular-2-failed-unknown-error-angular-is-not-defined
+    browser.ignoreSynchronization = true;
+    browser.get('#/pages/dashboard');
+    expect(browser.getCurrentUrl()).toContain('#/login');
 
-    element(by.model('username')).sendKeys('root')
-    element(by.model('password')).sendKeys('testing')
+    element(by.id('inputUsername')).sendKeys('root')
+    element(by.id('inputPassword3')).sendKeys('testing')
 
     element(by.css('[type="submit"]')).click();
 
     expect(browser.getCurrentUrl()).toContain('#/pages/dashboard');
+    browser.ignoreSynchronization = false;
   });
 });
