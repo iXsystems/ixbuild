a0245532e6021fdc725fc8448bab3a43d752a860
christiand992 <christian@ixsystems.com>
2017-04-28 13:29:57 -0400

    Adding ui tests for accounts, filling out services boilerplate

--- freenas/ui-tests/angular/accounts.js
+++ freenas/ui-tests/angular/accounts.js
@@ -1,74 +1,82 @@
-accounts = new Object();
+// System accounts: users and groups
 
-accounts.user_list = [
-  {
-    'username': 'root',
-    'password': 'testing'
-  }
-];
+// http://stackoverflow.com/questions/36201691/protractor-angular-2-failed-unknown-error-angular-is-not-defined
+browser.ignoreSynchronization = true;
 
-accounts.default_user = function(){
+var accounts = new Object();
+
+accounts.user_list = function() {
+  return new Array(
+    {
+      'username': 'root',
+      'password': 'testing'
+    }
+  );
+};
+
+// our default user will be the `root` account
+accounts.default_user = function() {
   return this.user_list[0];
 };
 
-accounts.login = function(){
+accounts.login = function(user) {
 
-  var user = this.default_user();
+  // using our default `root` account unless otherwise passed as an arg
+  var _user = typeof user Object ? user : this.default_user();
 
-  describe('angularjs login', function() {
-    it('should authenticate default user', function() {
-      // http://stackoverflow.com/questions/36201691/protractor-angular-2-failed-unknown-error-angular-is-not-defined
-      browser.ignoreSynchronization = true;
-      browser.get('#/pages/dashboard');
-      expect(browser.getCurrentUrl()).toContain('#/login');
+  describe('logging in as our root account ', function() {
+    it('should authenticate the user and re-direct to the dashboard', function() {
+      var username = element(by.id('inputUsername'));
+      var password = element(by.id('inputPassword3'));
 
-      element(by.id('inputUsername')).sendKeys(user.username)
-      element(by.id('inputPassword3')).sendKeys(user.password)
+      username.clear();
+      username.sendKeys(_user.username);
+
+      password.clear();
+      password.sendKeys(_user.password);
 
       element(by.css('[type="submit"]')).click();
 
       expect(browser.getCurrentUrl()).toContain('#/pages/dashboard');
-      browser.ignoreSynchronization = false;
     });
   });
+
+  return true;
 };
 
-accounts.logout = function(){
-  describe('angularjs logout', function() {
-    it('should sign out the default user', function() {
-      // http://stackoverflow.com/questions/36201691/protractor-angular-2-failed-unknown-error-angular-is-not-defined
-      browser.ignoreSynchronization = true;
-
-      // verify we are logged in, thus redirected to the dashboard from /login
-      browser.get('#/login');
-      expect(browser.getCurrentUrl()).toContain('#/pages/dashboard');
-
-      // logout
+accounts.logout = function() {
+  describe('logging out', function() {
+    it('should re-direct to the login page with empty username/password input fields', function() {
       browser.get('#/pages/dashboard');
-      element(by.css('[type="submit"]')).click();
-      expect(browser.getCurrentUrl()).toContain('#/login');
 
-      // ensure we can no longer access the dashboard
-      browser.get('#/pages/dashboard')
-      expect(browser.getCurrentUrl()).toContain('#/login');
+      element(by.id('user-profile-dd')).click();
+      element(by.css('.signout')).click();
 
-      browser.ignoreSynchronization = false;
+      expect(browser.getCurrentUrl()).toContain('#/login');
+      expect(element(by.id('inputUsername')).getAttribute('value')).toEqual("");
     });
   });
+
+  return true;
 };
 
 
-accounts.setUp = function(){
+accounts.setUp = function() {
   accounts.login();
 };
 
-accounts.tearDown = function(){
+accounts.tearDown = function() {
   accounts.logout();
 };
 
-// Group tests
+account.tests = function() {
+  var self = this;
 
-// User and Group with filesystem tests
-//  -- Dataset
-//  -- LDAP
-//  -- Shares
+  expect(self.login()).toEqual(true);
+  expect(self.logout()).toEqual(true);
+
+  return true;
+};
+
+// restore previous value
+browser.ignoreSynchronization = false;
