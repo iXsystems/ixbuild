523433727eb257414db1b19376e69085cfacc3ce
Joe Maloney <jmaloney@Joes-iMac.local>
2017-04-30 09:56:14 -0400

    Correct example location in warning

--- functions.sh
+++ functions.sh
@@ -1168,7 +1168,7 @@ jenkins_freenas_tests_jailed()
   . ${PROGDIR}/config/${BUILDTAG}.conf
   # Until py-iocage supports ip4start/ip4end properties again, or dhcp we must require an interface,IP address, and netmask
   if [ -z "$ip4_addr" ] ; then
-    echo "You must specify interfaces ip addresses, and netmasks for jails in executors/${BUILDTAG.conf}!"
+    echo "You must specify interfaces ip addresses, and netmasks for jails in ${PROGDIR}/config/${BUILDTAG}.conf"
     echo '"example: ip4_addr="igb0|192.168.58.7/24,igb1|10.20.20.7/23"'
     exit 1
   fi
