934a9206fdb0b8fb06b229cae05659d8fd934bd0
christiand992 <christian@ixsystems.com>
2017-05-04 09:08:31 -0400

    Fixing ui-tests run.sh script to handle arguments

--- freenas/ui-tests/run.sh
+++ freenas/ui-tests/run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 CURRDIR="$(realpath "$(dirname "$0")")"
 
+CMD=$1
+MODULE=$2
+
 source "${CURRDIR}/../../build.conf"
 if [ -f "${CURRDIR}/config.local" ]; then
   source "${CURRDIR}/config.local"
@@ -8,19 +11,31 @@ else
   source "${CURRDIR}/config.prod"
 fi
 
-TESTSUITES=( "dojo" "angular" )
+runtests()
+{
+  TESTSUITES=( "dojo" "angular" )
 
-# Allow argument for running a single version of the testsuite
-if [ -n "$1" ]; then
-  if [ -d "${CURRDIR}/$1" ]; then
-    TESTSUITES=( "$1" )
-  else
-    echo "No test suite found for \"$1\". Available test suites: ${TESTSUITES[@]}"
-    exit 1
+  # Allow argument for running a single version of the testsuite
+  if [ -n "$1" ]; then
+    if [ -d "${CURRDIR}/$1" ]; then
+      TESTSUITES=( "$1" )
+    else
+      echo "No test suite found for \"$1\". Available test suites: ${TESTSUITES[@]}"
+      exit 1
+    fi
   fi
-fi
 
-for testsuite in ${TESTSUITES[@]}
-do
-  "${CURRDIR}/${testsuite}/runtests.sh"
-done
+  for testsuite in ${TESTSUITES[@]}
+  do
+    "${CURRDIR}/${testsuite}/runtests.sh"
+  done
+
+  return 0
+}
+
+case "${CMD,,}" in
+  tests)  runtests ;;
+angular)  runtests "angular" ;;
+   dojo)  runtests "dojo" ;;
+      *) echo "Usage: $0 [tests|dojo|angular]" && exit 1 ;;
+esac
