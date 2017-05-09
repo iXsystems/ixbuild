d51c46fa4f8e07f9ac3d6a4e896f4cd0c29a222f
christiand992 <christian@ixsystems.com>
2017-05-09 11:39:13 -0400

    Updates service tests to start and stop the majority of the services
    * Moves to single start and stop function to control service by name.
    * Fixes issue with scrolling/focusing on page element that has overlaying element.
    * Replaces arbitrary sleep with event hook when waiting for service to start/stop.
    * Verifies service status once started.

--- freenas/ui-tests/angular/services.js
+++ freenas/ui-tests/angular/services.js
@@ -6,88 +6,119 @@ const accounts = require('./accounts.js');
 
 var services = new Object();
 
-// Each services.${servicename} should have an interface of:
-//    start(), stop(), setUp(), tearDown(), tests()
-services.webdav = new Object();
+// Service name: Order in which found in the services view
+// Note: currently skipping some services due to first requiring configuration
+services.service_idx = {
+  'afp': 1,
+  'smb': 2,
+  'ftp': 5,
+  'iscsi': 6,
+  'lldp': 7,
+  'nfs': 8,
+  'rsync': 9,
+  's3': 10,
+  'snmp': 12,
+  'ssh': 13,
+  'tftp': 14,
+  'webdav': 16
+};
 
-services.webdav.start = function() {
-  describe('webdav service', function() {
+// Starts a service by the service name (services.service_idx)
+services.start_service_by_name = function(servicename) {
+  var idx = services.service_idx[servicename];
+
+  describe(servicename + ' service', function() {
     it('should start', function() {
-      browser.get('#/pages/services');
+      browser.driver.getCurrentUrl().then(function(actualUrl) {
+        if (actualUrl.indexOf('#/pages/services') == -1) {
+          browser.get('#/pages/services');
+        }
+      });
       browser.wait(protractor.ExpectedConditions.urlContains('#/pages/services'), 6000);
       browser.wait(protractor.ExpectedConditions.presenceOf($('button.btn.btn-primary')), 30000);
+      // protractor doesn't take into account the scrolling div.page-top element when scrolling
+      // from the bottom of the view to focus on the first service element. This results on the
+      // div.page-top element covering the top-most service's row, thus we scroll to the top of the
+      // page when working with the top-most service element.
+      if (idx == 1) browser.driver.executeScript('window.scrollTo(0, 0);');
 
-      let btn_el = $$('service').get(15).$$('div > button').first();
-      btn_el.isDisplayed().then(function(isVisable) {
+      let btn = $('service:nth-child(' + idx + ') div.card-body > div.row > div:nth-child(3) > button');
+      let status_el = $('service:nth-child(' + idx + ') div.card-body > div.row > div:nth-child(2) > span');
+      btn.isDisplayed().then(function(isVisable) {
         if (isVisable) {
-          btn_el.click();
-
-          // @TODO - figure out a way to wait for the 'loading' dialog to complete
-          // while waiting for the service to start instead of an arbitrary sleep()
-          browser.driver.sleep('5000');
-          expect(btn_el.getText()).toEqual('Stop');
+          expect(status_el.getText()).toEqual('STOPPED');
+          expect(btn.getText()).toEqual('Start');
+          btn.click();
+          browser.wait(protractor.ExpectedConditions.invisibilityOf($('service:nth-child(' + idx +') div.ng-busy')), 15000);
+          expect(btn.getText()).toEqual('Stop');
+          expect(status_el.getText()).toEqual('RUNNING');
         }
       });
     });
   });
-
   return true;
 };
 
-services.webdav.stop = function() {
-  describe('webdav service', function() {
+// Stops a service by the service name (services.service_idx)
+services.stop_service_by_name = function(servicename) {
+  var idx = services.service_idx[servicename];
+
+  describe(servicename + ' service', function() {
     it('should stop', function() {
-      browser.get('#/pages/services');
+      browser.driver.getCurrentUrl().then(function(actualUrl) {
+        if (actualUrl.indexOf('#/pages/services') == -1) {
+          browser.get('#/pages/services');
+        }
+      });
       browser.wait(protractor.ExpectedConditions.urlContains('#/pages/services'), 6000);
-      browser.wait(protractor.ExpectedConditions.presenceOf($('button.btn.btn-primary')), 30000);
+      browser.wait(protractor.ExpectedConditions.presenceOf($('button.btn.btn-primary')), 30000); 
+      // protractor doesn't take into account the scrolling div.page-top element when scrolling
+      // from the bottom of the view to focus on the first service element. This results on the
+      // div.page-top element covering the top-most service's row, thus we scroll to the top of the
+      // page when working with the top-most service element.
+      if (idx == 1) browser.driver.executeScript('window.scrollTo(0, 0);');
 
-      var btn_el = $$('service').get(15).$$('div > button').first();
-      btn_el.isDisplayed().then(function(isVisable) {
+      let btn = $('service:nth-child(' + idx + ') div.card-body > div.row > div:nth-child(3) > button');
+      let status_el = $('service:nth-child(' + idx + ') div.card-body > div.row > div:nth-child(2) > span');
+      btn.isDisplayed().then(function(isVisable) {
         if (isVisable) {
-          btn_el.click();
-
-          // @TODO - figure out a way to wait for the 'loading' dialog to complete
-          // while waiting for the service to stop instead of an arbitrary sleep()
-          browser.driver.sleep('5000');
-          expect(btn_el.getText()).toEqual('Start');
+          expect(status_el.getText()).toEqual('RUNNING');
+          expect(btn.getText()).toEqual('Stop');
+          btn.click();
+          browser.wait(protractor.ExpectedConditions.invisibilityOf($('service:nth-child(' + idx +') div.ng-busy')), 15000);
+          expect(btn.getText()).toEqual('Start');
+          expect(status_el.getText()).toEqual('STOPPED');
         }
       });
     });
   });
-
   return true;
 };
 
-services.webdav.setUp = function() {
-  // start service
-  // create dataset for share
-  // create webdav share
-  // verify webdav share access, read/write (?)
-  // TODO: storage.create(dataset_name, dataset_type);
+// Log in and then start all of the services
+services.setUp = function() {
   accounts.login();
-  services.webdav.start();
+  for (var servicename in services.service_idx) {
+    if (services.service_idx.hasOwnProperty(servicename)) {
+      services.start_service_by_name(servicename);
+    }
+  }
 };
 
-services.webdav.tearDown = function() {
-  // stop service
-  // destroy dataset for share
-  //services.webdav.stop();
-  // TODO: storage.destroy(dataset_name, dataset_type);
-  services.webdav.stop();
+// Stop all of the services and then log out
+services.tearDown = function() {
+  for (var servicename in services.service_idx) {
+    if (services.service_idx.hasOwnProperty(servicename)) {
+      services.stop_service_by_name(servicename);
+    }
+  }
   accounts.logout();
 };
 
-services.webdav.tests = function() {
-  var self = this;
-
-  self.setUp();
-  self.tearDown();
-
-  return true;
-};
-
+// Run all of the service tests
 services.tests = function() {
-  services.webdav.tests();
+  this.setUp();
+  this.tearDown();
 };
 
 module.exports = services;
