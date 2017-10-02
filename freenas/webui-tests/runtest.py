2751fabd78190e0d6402cf19f041dfad47b24890
ericbsd <ericturgeon.bsd@gmail.com>
2017-10-02 17:16:09 -0300

    Added if statment to only delete file if it exist to avoid the script to return 0

--- freenas/webui-tests/runtest.py
+++ freenas/webui-tests/runtest.py
@@ -1,11 +1,16 @@
 from login import *
 from subprocess import call
+from os import path
 
 #starting the test and genewratinf result
 call(["py.test", "--junitxml", "/temp/result.xml", "login.py"])
 
 #cleaning up files
-call(["rm", "login.pyc"])
-call(["rm", "source.pyc"])
-call(["rm", "-r", "__pycache__"])
-call(["rm", "-r", ".cache"])
+if path.exists('login.pyc'):
+    call(["rm", "login.pyc"])
+if path.exists('source.pyc'):
+    call(["rm", "source.pyc"])
+if path.exists('__pycache__'):
+    call(["rm", "-r", "__pycache__"])
+if path.isdir('.cache'):
+    call(["rm", "-r", ".cache"])
