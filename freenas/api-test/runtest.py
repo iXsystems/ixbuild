ebecd69f54a2b665aa47d4fb23395e6a71d3b1db
ericbsd <ericturgeon.bsd@gmail.com>
2017-10-26 10:33:52 -0300

    Change disk name from ada to vtbd

--- freenas/api-test/runtest.py
+++ freenas/api-test/runtest.py
@@ -51,8 +51,8 @@ freenas_url = 'http://' + ip + '/api/v1.0'
 interface = "%s"
 ntpServer = "10.20.20.122"
 localHome = "%s"
-disk1 = "ada1"
-disk2 = "ada2"
+disk1 = "vtbd1"
+disk2 = "vtbd2"
 keyPath = "%s"
 """ % (passwd, ip, interface, localHome, keyPath)
 
