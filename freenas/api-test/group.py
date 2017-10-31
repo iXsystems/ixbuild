9c7b8b0c7fbdd432f8e50f1f3e513fb7a1206c2c
ericbsd <ericturgeon.bsd@gmail.com>
2017-10-31 10:38:04 -0300

    Add code for group.py

---
+++ freenas/api-test/group.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.6
+
+# Author: Eric Turgeon
+# License: BSD
+# Location for tests into REST API of FreeNAS
+
+import unittest
+from functions import POST, GET_USER
+
+
+class group_test(unittest.TestCase):
+
+    def test_01_Creating_home_dataset_tank_sur_testuser(self):
+        payload = {"bsdgrp_gid": 1200,"bsdgrp_group": "testgroup"}
+        assert POST("/account/groups/", payload) == 201
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
