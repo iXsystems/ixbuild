06841b919383501bbe473dc9ec6797c7a54e7eb5
ericbsd <ericturgeon.bsd@gmail.com>
2017-11-01 14:25:46 -0300

    Adding ldap_bsd.py

--- freenas/api-test/iscsi.py
+++ freenas/api-test/iscsi.py
@@ -24,7 +24,7 @@ class iscsi_test(unittest.TestCase):
         assert POST("/services/iscsi/target/", payload) == 201
 
     # Add Target to groups
-    def test_04_Add_target_to_groups():
+    def test_04_Add_target_to_groups(self):
         payload = {"iscsi_target": "1",
                    "iscsi_target_authgroup": "null",
                    "iscsi_target_portalgroup": 1,
@@ -35,7 +35,7 @@ class iscsi_test(unittest.TestCase):
         assert POST("/services/iscsi/targetgroup/", payload) == 201
 
     # Add iSCSI extent
-    def test_05_Add_ISCSI_extent():
+    def test_05_Add_ISCSI_extent(self):
         payload = {"iscsi_target_extent_type": "File",
                    "iscsi_target_extent_name": "extent",
                    "iscsi_target_extent_filesize": "50MB",
@@ -44,18 +44,18 @@ class iscsi_test(unittest.TestCase):
         assert POST("/services/iscsi/extent/", payload) == 201
 
     # Associate iSCSI target
-    def test_06_Associate_ISCSI_target():
+    def test_06_Associate_ISCSI_target(self):
         payload = {"id": "1", "iscsi_extent": "1",
                    "iscsi_lunid": "null",
                    "iscsi_target": "1" }
         assert POST("/services/iscsi/targettoextent/", payload) == 201
 
     # Enable the iSCSI service
-    def test_07_Enable_iSCSI_service():
+    def test_07_Enable_iSCSI_service(self):
         payload = {"srv_enable": "true"}
         assert PUT("/services/services/iscsitarget/", payload) == 200
 
-    def test_08_Verify_the_iSCSI_service_is_enabled();
+    def test_08_Verify_the_iSCSI_service_is_enabled(self);
         assert GET_OUTPUT("/services/services/iscsitarget/", "srv_state") == "RUNNING"
 
 
--- freenas/api-test/jails.py
+++ freenas/api-test/jails.py
@@ -10,40 +10,39 @@ from functions import PUT, POST, GET_OUTPUT
 
 class jails_test(unittest.TestCase):
 
-
-    def test_01_Configuring_jails():
+    def test_01_Configuring_jails(self):
         payload = {"jc_ipv4_network_start": JAILIP,
                    "jc_path": "/mnt/tank/jails" }
         assert PUT("/jails/configuration/", payload) == 201
         # Timeout if jail creation hangs
   .     #${PROGDIR}/../utils/resty -W "http://${ip}:80/api/v1.0" -H "Accept: application/json" -H "Content-Type: application/json" -u ${fuser}:${fpass} -m 1200
 
-    def test_02_Creating_jail_VNET_OFF():
+    def test_02_Creating_jail_VNET_OFF(self):
         payload == {"jail_host": "testjail",
                     "jail_defaultrouter_ipv4": JAILGW,
                     "jail_ipv4": JAILIP,
                     "jail_ipv4_netmask": JAILNETMASK,
-                    "jail_vnet": "false" }
+                    "jail_vnet": "false"}
         assert POST("/jails/jails/", payload) == 201
 
         # Remove timeout for other tests
   .     # ${PROGDIR}/../utils/resty -W "http://${ip}:80/api/v1.0" -H "Accept: application/json" -H "Content-Type: application/json" -u ${fuser}:${fpass}
 
-    def test_03_Mount_tank_share_into_jail():
+    def test_03_Mount_tank_share_into_jail(self):
         payload == {"destination": "/mnt",
                      "jail": "testjail",
                      "mounted": "true",
                      "readonly": "false",
-                     "source": "/mnt/tank/share" }
+                     "source": "/mnt/tank/share"}
         assert POST("/jails/mountpoints/", payload) == 201
 
-    def test_04_Starting_jail():
+    def test_04_Starting_jail(self):
         assert POST("/jails/jails/1/start/", "") == 202
 
-    def test_05_Restarting_jail():
+    def test_05_Restarting_jail(self):
         assert POST("/jails/jails/1/restart/", "") == 202
 
-    def test_06_Stopping_jail():
+    def test_06_Stopping_jail(self):
         assert POST("/jails/jails/1/stop/", "") == 202
 
 if __name__ == "__main__":
