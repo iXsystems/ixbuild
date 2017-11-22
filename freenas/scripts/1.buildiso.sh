77a22c2f78e4ddec2c3a866de04a694d8b236f27
Kris Moore <kris@ixsystems.com>
2017-11-22 10:12:23 -0500

    Clean more source repos on failed "make checkout"

--- freenas/scripts/1.buildiso.sh
+++ freenas/scripts/1.buildiso.sh
@@ -50,7 +50,7 @@ parse_build_error()
 clean_src_repos()
 {
   # Clean out source repos which tend to get cranky on force-push
-  cRepos="webui freenas samba ports iocage os"
+  cRepos="webui freenas samba ports iocage os py-licenselib py-bsd py-libzfs freenas-pkgtools freenas-docs"
   for r in $cRepos
   do
     if [ -d "${PROFILE}/_BE/${r}" ] ; then
